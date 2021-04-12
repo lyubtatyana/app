5499a0e529c77eeafbda09447a44d9d9d902a60d
Lyubchik Tatyana <lyubtatyana@mail.ru>
2021-04-12 22:01:32 +0500

    task js-1 решение задач js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//задача 1
+const sum = (a, b) => a + b;
+const result  = 55 * sum(7, 2) / sum(4, 2)
+console.log (result);
+
+//задача 2
+let str = 'Таня';
+console.log(str);
+
+const birth = '15.09.1994';
+console.log(birth);
+
+const message = (name) => {
+    console.log("Привет", name);
+};
+message("Таня");
+
+//задача 3
+
+const average = (a, b) => (a + b) / 2;
+console.log(average(2, 3));
+
+const square = (x) => x ** 2;
+console.log(square(4));
+
+const cube = (x) => x ** 3;
+console.log(cube(6));
+
+//дополнительная задача
+
+const square = (x) => x ** 2;
+const cube = (x) => x ** 3;
+const average = (a, b) => (a + b) / 2;
+
+const calc = () => {
+    const result = [x];
+
+    for (let x = 0; x < 10; x++) {
+     const result = calc[x];   
+}
+    return result;
+};
+
+console.log(calc);
